dc987225de31d8f3a47982cdf8df011a7cb4a506
hero2bash <zero.bash@gmail.com>
2018-06-14 14:30:00 +0800

    Add subscription to user conversation

--- docs/client_app.js
+++ docs/client_app.js
@@ -1,6 +1,9 @@
 /*
     This sample uses ES6 coding standards.
 */
+let clientApp = {};
+
+// Will Authenticate through PureCloud and subscribe to User Conversation Notifications
 let setup = function(){
     // PureCloud OAuth information
     const platformClient = require('platformClient');
@@ -8,7 +11,6 @@ let setup = function(){
     const clientId = "8c821827-57bd-4d44-8765-597d4a3220c5";
     const redirectUri = "http://localhost:3000";
     //const redirectUri = "https://princemerluza.github.io/purecloud-premium-app/";
-
 
     // API instances
     const usersApi = new platformClient.UsersApi();
@@ -21,15 +23,36 @@ let setup = function(){
         console.log(data);
         // Set access Token
         client.setAccessToken(data.accessToken);
+       
         
+    // Get Details of current User and save to Client App
+        return usersApi.getUsersMe();
+    })
+    .then( userMe => {
+        clientApp.userId = userMe.id;
+
+
+    // Create a Notifications Channel
         return notificationsApi.postNotificationsChannels();
     })
-    // Create a new Channel and Subscribe to Changes
-    .then( data => console.log(data) )
-    .catch( e => console.log(err) );
-}
+    .then(data => {
+        clientApp.websocketUri = data.connectUri;
+        clientApp.channelID = data.id;
 
-let clientApp = {};
+
+    // Subsccribe to Call Conversations of Current user.
+        let topic = [{
+            "id": "v2.users." + clientApp.userId + ".conversations.calls"
+        }];
+        return notificationsApi.postNotificationsChannelSubscriptions(clientApp.channelID, topic);
+    })
+    .then(data => console.log(data))
+
+    // Catch for errors
+    .catch( e => console.log(e) );
+}
+//return notificationsApi.postNotificationsChannels();
+
 clientApp.setup = setup;
 
 export default clientApp
