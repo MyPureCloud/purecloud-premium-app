8e68de99ee128838f84fe15ae98d7d3a8e05cd16
hero2bash <zero.bash@gmail.com>
2018-06-25 09:00:53 +0800

    Set default value for clientId to mypurecloud.com

--- docs/client_app.js
+++ docs/client_app.js
@@ -12,9 +12,10 @@ clientApp.setup = function(pcEnv){
     // PureCloud OAuth information
     const platformClient = require('platformClient');
     const client = platformClient.ApiClient.instance;
-    const clientId = clientIDs[pcEnv];
     // const redirectUri = "http://localhost:3000";
     const redirectUri = "https://princemerluza.github.io/purecloud-premium-app/";
+
+    let clientId = clientIDs[pcEnv] || 'mypurecloud.com';
 
     test = clientId;
 
