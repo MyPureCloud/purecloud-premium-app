1622f4374ffe386ff64f92c8c7db336c907331b5
hero2bash <zero.bash@gmail.com>
2018-06-26 17:54:22 +0800

    Add https for localhost testing

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
+/*
+* Copyright (c) 2015 Interactive Intelligence
+*
+* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+var http = require("http");
+var https = require("https"),
+    fs = require("fs");
 var express = require('express');
 var app = express();
 
-app.use(express.static('docs'));
-app.get('/', function(req,res){
-    res.sendFile(__dirname + '/docs/index.html');
-});
+app.use(express.static(__dirname+ "/docs"));
 
-app.listen(3000);
+app.set('port', 3000);
+
+var sslOptions = {
+    key: fs.readFileSync('https-requirements/_localhost.key'),
+    cert: fs.readFileSync('https-requirements/_localhost.crt'),
+    requestCert: true,
+    rejectUnauthorized: false
+};
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(sslOptions, app);
+
+var httpsPort = 443;
+
+console.log("starting on " + httpsPort + ' (https)');
+httpServer.listen(app.get('port'));
+httpsServer.listen(httpsPort);
