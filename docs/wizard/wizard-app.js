ace86842187d42e018ca41cbd3b02b3f9d0b7814
hero2bash <zero.bash@gmail.com>
2018-06-27 12:24:29 +0800

    Add user permission check

--- docs/wizard/wizard-app.js
+++ docs/wizard/wizard-app.js
@@ -3,12 +3,19 @@ import clientIDs from '../clientIDs.js'
 let wizardApp = {
     // Reference to the PureCloud App
     pcApp: null,
+    platformClient: null,
+    purecloudClient: null,
+    setupPermissionsRequired: ['admin'],
 
     // Functions
     setup: undefined,
     pureCloudAuthenticate: undefined
 }
+// Aliases
 let pcApp = wizardApp.pcApp;
+let platformClient = wizardApp.wizardApp;
+let purecloudClient = wizardApp.purecloudClient;
+let setupPermissionsRequired = wizardApp.setupPermissionsRequired;
 
 wizardApp.setup = function(){
     // Backwards compatibility snippet from: https://github.com/MyPureCloud/client-app-sdk
@@ -27,18 +34,33 @@ wizardApp.setup = function(){
 wizardApp.pureCloudAuthenticate = function(){
     // Authenticate through PureCloud
     const redirectUri = "https://localhost/wizard/index.html";
-    const platformClient = require('platformClient');
-    let purecloudClient = platformClient.ApiClient.instance;
+    platformClient = require('platformClient');
+    purecloudClient = platformClient.ApiClient.instance;
+    purecloudClient.setPersistSettings(true, 'premium_app');
 
     purecloudClient.loginImplicitGrant(clientIDs[pcApp.pcEnvironment], 
                             redirectUri, 
                             {state: ('pcEnvironment=' + pcApp.pcEnvironment)})
-    .then(function(data) {
+    // Check permission of user
+    .then(data => {
         console.log(data);
-    })
-    .catch(function(err) {
-        console.log(err);
-    });
+
+        let usersApi = new platformClient.UsersApi();
+        let opts = {'expand': ['authorization']};
+        return usersApi.getUsersMe(opts); 
+    
+    }).then(userMe => {
+        console.log(userMe);
+        // Show appropriate elements based on qualification of user permissions.
+        if(!setupPermissionsRequired.every(perm => userMe.authorization.permissions.indexOf(perm) > -1)){
+            $('#unauthorized').show();
+            $('#authorized').hide();
+        }else{
+            $('#unauthorized').hide();
+            $('#authorized').show();
+        }
+        
+    }).catch(err => console.log(err));
 }
 
 export default wizardApp
