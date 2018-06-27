b251b88dab980bccd948ea21745ffba0001c197c
hero2bash <zero.bash@gmail.com>
2018-06-27 13:28:35 +0800

    Convert wizardApp to ES6 Class syntax

--- docs/wizard/wizard-app.js
+++ docs/wizard/wizard-app.js
@@ -1,3 +1,6 @@
+/*
+*   This sample uses ES6 features
+*/
 import clientIDs from '../clientIDs.js'
 
 // Requires jQuery and Handlebars from parent context
@@ -12,93 +15,82 @@ if((typeof $ === 'undefined') ||
                   "==============================");   
 }
 
-
-let wizardApp = {
-    // Reference to the PureCloud App (Client App SDK)
-    pcApp: null,
-    // PureCloud Javascript SDK clients
-    platformClient: null,
-    purecloudClient: null,
-    // Permissions required for using the app 
-    // TODO: store permissions on a separate file
-    setupPermissionsRequired: ['admin'],
-    // Handlebars values
-    templateSource: null,
-    template: null,
-
-    // Functions
-    setupClientApp: () => null,
-    pureCloudAuthenticate: () => null,
-    renderPage: (page) => null
-}
-// Aliases
-let _pcApp = wizardApp.pcApp;
-let _platformClient = wizardApp.wizardApp;
-let _purecloudClient = wizardApp.purecloudClient;
-let _setupPermissionsRequired = wizardApp.setupPermissionsRequired;
-let _templateSource = wizardApp.templateSource;
-let _template = wizardApp.template;
-
-wizardApp.setupClientApp = function(){
-    _templateSource = document.getElementById("wizard-app-template").innerHTML;
-    _template = Handlebars.compile(_templateSource);
-
-    // Backwards compatibility snippet from: https://github.com/MyPureCloud/client-app-sdk
-    let envQueryParamName = 'pcEnvironment';
-
-    if (window && window.location && typeof window.location.search === 'string' &&
-        window.location.search.indexOf('pcEnvironment') >= 0) {
-        _pcApp = new window.purecloud.apps.ClientApp({pcEnvironmentQueryParam: envQueryParamName});
-    } else {
-        // Use default PureCloud region
-        _pcApp = new window.purecloud.apps.ClientApp();
+class WizardApp {
+    constructor(){
+        // Reference to the PureCloud App (Client App SDK)
+        this.pcApp = null;
+        // PureCloud Javascript SDK clients
+        this.platformClient = null;
+        this.purecloudClient = null;
+        // Permissions required for using the app 
+        // TODO: store permissions on a separate file
+        this.setupPermissionsRequired = ['admin'];
+        // Handlebars values
+        this.templateSource = null;
+        this.template = null;
     }
-    console.log(_pcApp.pcEnvironment);
-}
 
-wizardApp.pureCloudAuthenticate = function(){
-    // Authenticate through PureCloud
-    const redirectUri = "https://localhost/wizard/index.html";
-    _platformClient = require('platformClient');
-    _purecloudClient = _platformClient.ApiClient.instance;
-    _purecloudClient.setPersistSettings(true, 'premium_app');
-
-    _purecloudClient.loginImplicitGrant(clientIDs[_pcApp.pcEnvironment], 
-                            redirectUri, 
-                            {state: ('pcEnvironment=' + _pcApp.pcEnvironment)})
-    // Check permission of user
-    .then(data => {
-        console.log(data);
-
-        let usersApi = new _platformClient.UsersApi();
-        let opts = {'expand': ['authorization']};
-        return usersApi.getUsersMe(opts); 
+    setupClientApp(){
+        this.templateSource = document.getElementById("wizard-app-template").innerHTML;
+        this.template = Handlebars.compile(this.templateSource);
     
-    }).then(userMe => {
-        console.log(userMe);
-        // Show appropriate elements based on qualification of user permissions.
-        if(!_setupPermissionsRequired.every(perm => userMe.authorization.permissions.indexOf(perm) > -1)){
-            $('#unauthorized').show();
-            $('#authorized').hide();
-        }else{
-            $('#unauthorized').hide();
-            $('#authorized').show();
+        // Backwards compatibility snippet from: https://github.com/MyPureCloud/client-app-sdk
+        let envQueryParamName = 'pcEnvironment';
+    
+        if (window && window.location && typeof window.location.search === 'string' &&
+            window.location.search.indexOf('pcEnvironment') >= 0) {
+                this.pcApp = new window.purecloud.apps.ClientApp({pcEnvironmentQueryParam: envQueryParamName});
+        } else {
+            // Use default PureCloud region
+            this.pcApp = new window.purecloud.apps.ClientApp();
         }
-        
-    }).catch(err => console.log(err));
-}
+        console.log(this.pcApp.pcEnvironment);
+    }
 
-wizardApp.renderPage = function(page){
-    switch(page){
-        case 'landing_page':
-            let context = {
-                title: 'App Setup Wizard',
-                subtitle: 'Welcome! This Wizard will assist you in the installation, modification, or removal of the Premium App.'
+    pureCloudAuthenticate() {
+        // Authenticate through PureCloud
+        const redirectUri = "https://localhost/wizard/index.html";
+        this.platformClient = require('platformClient');
+        this.purecloudClient = this.platformClient.ApiClient.instance;
+        this.purecloudClient.setPersistSettings(true, 'premium_app');
+    
+        this.purecloudClient.loginImplicitGrant(clientIDs[this.pcApp.pcEnvironment], 
+                                redirectUri, 
+                                {state: ('pcEnvironment=' + this.pcApp.pcEnvironment)})
+        // Check permission of user
+        .then(data => {
+            console.log(data);
+    
+            let usersApi = new this.platformClient.UsersApi();
+            let opts = {'expand': ['authorization']};
+            return usersApi.getUsersMe(opts); 
+        
+        }).then(userMe => {
+            console.log(userMe);
+            // Show appropriate elements based on qualification of user permissions.
+            if(!this.setupPermissionsRequired.every(perm => userMe.authorization.permissions.indexOf(perm) > -1)){
+                $('#unauthorized').show();
+                $('#authorized').hide();
+            }else{
+                $('#unauthorized').hide();
+                $('#authorized').show();
             }
-            let renderedHtml = _template(context);
-            $('#wizard-app-display').html(renderedHtml);
-            break;
+            
+        }).catch(err => console.log(err));
+    }
+
+    renderPage(page) {
+        switch(page){
+            case 'landing_page':
+                let context = {
+                    title: 'App Setup Wizard',
+                    subtitle: 'Welcome! This Wizard will assist you in the installation, modification, or removal of the Premium App.'
+                }
+                let renderedHtml = this.template(context);
+                $('#wizard-app-display').html(renderedHtml);
+                break;
+        }
     }
 }
 
-export default wizardApp
+export default WizardApp
