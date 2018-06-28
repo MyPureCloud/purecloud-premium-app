da6539d4f3e8c76c4a8070d29c9f1f563747c0d3
AgnesCorpuz <38449514+AgnesCorpuz@users.noreply.github.com>
2018-06-28 10:04:29 +0800

    Fixed View

--- docs/client_app.js
+++ docs/client_app.js
@@ -118,7 +118,7 @@ clientApp.loadSupervisorView = function(){
 
         let dropdown = $('#ddlQueues');
         dropdown.empty();
-        dropdown.append('<option selected="true" disabled>Subscribe to Queue</option>');
+        dropdown.append('<option selected="true" disabled>Queues</option>');
         dropdown.prop('selectedIndex', 0);
 
         for (var i = 1; i < queues.length; i++) {
