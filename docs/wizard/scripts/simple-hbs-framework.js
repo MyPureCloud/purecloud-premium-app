a5a5d7f0e06f88129d6a413d088b076b76d668c6
hero2bash <zero.bash@gmail.com>
2018-07-03 17:40:50 +0800

    Rename file

--- docs/wizard/scripts/simple-hbs-framework.js
+++ docs/wizard/scripts/simple-hbs-framework.js
@@ -1,4 +1,13 @@
+/**
+ * This is a very simple Handlebars framework built
+ * specifically for the Premium App Sample.
+ * This will only handle the 'View'. There shouldn't be any 
+ * PureCloud or Data logic here.
+ */
+
 import hb from './template-references.js'
+
+let templatesPath = './views/';
 
 /**
  * Render the Handlebars template to the window
@@ -12,7 +21,7 @@ export function _renderModule(page, context, target, resolveData) {
     context = (typeof context !== 'undefined') ? context : {}; 
     target = (typeof target !== 'undefined') ? target : 'default-module'; 
 
-    let templateUri = './views/' + page + '.hbs';
+    let templateUri = templatesPath + page + '.hbs';
     let templateSource;
     let template;
 
@@ -58,6 +67,61 @@ export function _renderCompletePage(headerContext, bodyContext, bodyTemplate){
         .then(() => _renderModule(bodyTemplate, bodyContext, 'root-body'))
         .then(() => resolve())
     })
-    
+}
+
+export class Page {
+    constructor(onLoadScript, title, subtitle, bodyContext, bodyTemplate){
+        this.onLoadScript = onLoadScript;
+        this.title = title ? title : "Title";
+        this.subtitle = subtitle ? subtitle : "Subtitle";
+        this.bodyContext = bodyContext;
+        this.bodyTemplate = bodyTemplate;
+
+        this.modules = [];
+    }
+
+    load(){
+        let loadPageChain = function(){
+                _renderCompletePage(
+                {
+                    "title": this.title,
+                    "subtitle": this.subtitle
+                },
+                this.bodyContext,
+                this.bodyTemplate
+            )
+            .then(renderInternalModules);
+        }
+
+        // if onLoadScript is a Promise, wait for it
+        // before rendering the page.
+        if(typeof this.onLoadScript.then == 'function'){
+            this.onLoadScript()
+            .then(loadPageChain);
+        }else{
+            this.onLoadScript();
+            loadPageChain();
+        }
+    }
+
+    renderModule(moduleIndex){
+        return new Promise((resolve, reject) => {
+            this.modules[moduleIndex]()
+            .then(() => resolve())
+            .catch(() => reject());
+        });
+    }
+
+    renderInternalModules(){
+        return new Promise((resolve, reject) =>{
+            let promise = renderModule(0);
+            for(let i = 1; i < this.modules.length; i++){
+                promise = promise.then(renderModule(i));
+            }
+            promise
+            .then(() => resolve())
+            .catch(() => reject());
+        });
+    }
 }
 
