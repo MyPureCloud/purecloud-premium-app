ad174e64bc7e98c5fb4d8cb87a4611fa2e0a113d
AgnesCorpuz <38449514+AgnesCorpuz@users.noreply.github.com>
2018-07-18 11:15:32 +0800

    Updated Client IDs for testing

--- docs/clientIDs.js
+++ docs/clientIDs.js
@@ -1,5 +1,6 @@
 export default {
-    'mypurecloud.com': '8c821827-57bd-4d44-8765-597d4a3220c5', //Development Client ID
+    // 'mypurecloud.com': '8c821827-57bd-4d44-8765-597d4a3220c5', //Development Client ID
+    'mypurecloud.com': 'e88db007-526a-4f21-af25-81118183c37e', //Development Client ID    
     'mypurecloud.ie': 'c630c0e3-91f4-4184-bf48-765f87332ebf', //Development Client ID
     // 'mypurecloud.com': '26255f2c-6a85-43bf-8d27-7761057bc72d',
     // 'mypurecloud.ie': '939ab4dd-109f-4120-ba9f-051b973b9ecc',
