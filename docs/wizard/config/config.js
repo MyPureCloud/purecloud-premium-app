7ca1ad8e23a2c0c41603d13a5fb275f4b53b924b
hero2bash <zero.bash@gmail.com>
2020-04-15 09:32:17 +0800

    Add other regions

--- docs/wizard/config/config.js
+++ docs/wizard/config/config.js
@@ -2,8 +2,10 @@ export default {
     clientIDs: {        
         'mypurecloud.com': '26255f2c-6a85-43bf-8d27-7761057bc72d',
         'mypurecloud.ie': '939ab4dd-109f-4120-ba9f-051b973b9ecc',
+        'mypurecloud.de': 'aa8efb84-a77f-4c43-8b37-ac0566d9f73e',
         'mypurecloud.com.au': 'c8a4d721-3fbb-4f50-b3e0-aa49bf86ac87',
-        'mypurecloud.jp': '28dbeebd-8128-4fe0-8f42-f2eebb767a71'
+        'mypurecloud.jp': '28dbeebd-8128-4fe0-8f42-f2eebb767a71',
+        'usw2.pure.cloud': '2075921c-a285-4523-91df-7984f1268677'
     },
     // 'wizardUriBase': 'http://localhost:8080/wizard/',
     'wizardUriBase': 'https://mypurecloud.github.io/purecloud-premium-app/wizard/',
