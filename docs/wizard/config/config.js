0f2777cfc601498e9836fbbf6ad59021cc287f83
AgnesCorpuz <38449514+AgnesCorpuz@users.noreply.github.com>
2020-05-20 08:25:33 +0800

    Update US east Client ID

--- docs/wizard/config/config.js
+++ docs/wizard/config/config.js
@@ -1,6 +1,6 @@
 export default {
     clientIDs: {        
-        'mypurecloud.com': '26255f2c-6a85-43bf-8d27-7761057bc72d',
+        'mypurecloud.com': 'a4d0372b-918f-4954-a266-f3d9bb5d0ea8',
         'mypurecloud.ie': '939ab4dd-109f-4120-ba9f-051b973b9ecc',
         'mypurecloud.de': 'aa8efb84-a77f-4c43-8b37-ac0566d9f73e',
         'mypurecloud.com.au': 'c8a4d721-3fbb-4f50-b3e0-aa49bf86ac87',
